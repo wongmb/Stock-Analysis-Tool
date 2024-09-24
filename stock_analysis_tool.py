763c0e2ea1d494371485152ddf36897abf3d747d
wongm142 <wongm142@mcmaster.ca>
2024-09-23 21:02:43 -0400

    added timeframe

--- stock_analysis_tool.py
+++ stock_analysis_tool.py
@@ -20,14 +20,15 @@ client = Groq(
 
 def get_stock_price(ticker):
     try:
-        stockData = yf.Ticker(ticker).history(period="5d")
-        
+        stockData = yf.Ticker(ticker).history(period=st.session_state.plotPeriod)
+        st.session_state.firstPrice = stockData.Close.iloc[0]
+                
         # Check if the DataFrame is empty
         if stockData.empty:
             st.write("No data found for the given ticker. Please enter a valid company name.")
             return None
         
-        todaysData = stockData.Close.iloc[-1]  # Access the last closing price
+        todaysData = stockData.Close.iloc[-1] 
         return todaysData
     
     except Exception as e:
@@ -36,43 +37,51 @@ def get_stock_price(ticker):
 
 # Other functions
 def calculate_SMA(window):
-    data = yf.Ticker(st.session_state.ticker).history(period = "1y").Close
-    return (str(data.rolling(window = window).mean().iloc[-1]))
+    return (round(st.session_state.data.rolling(window = window).mean().iloc[-1], 2))
 
 def calculate_EMA(window):
-    data = yf.Ticker(st.session_state.ticker).history(period = "1y").Close
-    return (str(data.ewm(span = window, adjust = False).mean().iloc[-1]))
+    return (round(st.session_state.data.ewm(span = window, adjust = False).mean().iloc[-1], 2))
  
 def calculate_RSI():
-    data = yf.Ticker(st.session_state.ticker).history(period = "1y").Close
-    delta = data.diff()
+    delta = st.session_state.data.diff()
     up = delta.clip(lower = 0)
     down = -1 * delta.clip(upper = 0)
     ema_up = up.ewm(com=14 - 1, adjust = False).mean()
     ema_down = down.ewm(com=14 - 1, adjust = False).mean()
     rs = ema_up / ema_down
-    return str(100 - (100 / (1 + rs)).iloc[-1])
+    return round(100 - (100 / (1 + rs)).iloc[-1], 2)
 
 def calculate_MACD():
-    data = yf.Ticker(st.session_state.ticker).history(period = "1y").Close
-    short_EMA = data.ewm(span = 12, adjust = False).mean()
-    long_EMA = data.ewm(span = 26, adjust = False).mean()
+    short_EMA = st.session_state.data.ewm(span = 12, adjust = False).mean()
+    long_EMA = st.session_state.data.ewm(span = 26, adjust = False).mean()
     MACD = short_EMA - long_EMA
     signal = MACD.ewm(span = 9, adjust = False).mean()
     MACD_histogram = MACD - signal
+    MACD[-1] = round(MACD[-1], 2)
+    signal[-1] = round(signal[-1], 2)
+    MACD_histogram = round(MACD_histogram, 2)
     return (f"{MACD[-1]}, {signal[-1]}, {MACD_histogram[-1]}")
 
 def plot_stock_price():
-    data = yf.Ticker(st.session_state.ticker).history(period = "1y")
+    data = yf.Ticker(st.session_state.ticker).history(period = st.session_state.plotPeriod)
     plt.figure(figsize = (10, 5))
     plt.plot(data.index, data.Close)
-    plt.title(f"{st.session_state.ticker} Stock Price Over Last Year")
+    if st.session_state.plotName == "All Time":
+        plt.title(f"{st.session_state.companyName} Stock Price Over {st.session_state.plotName}")
+    else:
+        plt.title(f"{st.session_state.companyName} Stock Price Over Last {st.session_state.plotName}")
     plt.xlabel("Date")
-    plt.ylabel("Stock Price ($)")
+    plt.ylabel("Stock Price ($ USD)")
     plt.legend()
     plt.grid(True)
     plt.savefig("stock.png")
     plt.close()
+
+def calculatePercentageChange():
+    priceChange = st.session_state.stockPrice - st.session_state.firstPrice
+    percentageChange = (priceChange / st.session_state.firstPrice) * 100
+    
+    return round(percentageChange, 2)
     
 functions = [
     {
@@ -96,10 +105,19 @@ available_functions = {
 }
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+    st.session_state.messages = []
 
 if "currency" not in st.session_state:
     st.session_state.currency = "USD"
+
+if "plotPeriod" not in st.session_state:
+    st.session_state.plotPeriod = "1y"
+
+if "plotName" not in st.session_state:
+    st.session_state.plotName = "Year"
+
+if "firstPrice" not in st.session_state:
+    st.session_state.firstPrice = "None"
     
 with st.sidebar:
     user_input = st.text_input("Enter a Company Stock:", placeholder="e.g., Apple")
@@ -160,26 +178,57 @@ if user_input:
         st.write(f"Error fetching stock price: Please enter a valid Company name {e}")
         
 if "function_response" in st.session_state and st.session_state.function_response != None:    
+    st.session_state.data = yf.Ticker(st.session_state.ticker).history(period = "1y").Close
+
     currency = currencySelected.split(" ")
 
     st.session_state.stockPrice = round(c.convert(st.session_state.stockPrice, st.session_state.currency, currency[1]), 2)
     st.session_state.currency = currency[1]
     st.session_state.currencySymbol = currency[0]
     
-    st.metric(label = "Current Stock Price of " + st.session_state.ticker + " - " + st.session_state.companyName, value=f"{st.session_state.currencySymbol} {st.session_state.stockPrice}", delta="Change Info")
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    with col1:
+        if st.button("5D"):
+            st.session_state.plotName = "5 Days"
+            st.session_state.plotPeriod = "5d"
+    with col2:
+        if st.button("1M"):
+            st.session_state.plotName = "Month"
+            st.session_state.plotPeriod = "1mo"
+    with col3:
+        if st.button("6M"):
+            st.session_state.plotName = "6 Months"
+            st.session_state.plotPeriod = "6mo"
+    with col4:
+        if st.button("1Y"):
+            st.session_state.plotName = "Year"
+            st.session_state.plotPeriod = "1y"
+    with col5:
+        if st.button("5Y"):
+            st.session_state.plotName = "5 Years"
+            st.session_state.plotPeriod = "5y"
+    with col6:
+        if st.button("Max"):
+            st.session_state.plotPeriod = "max"
+            st.session_state.plotName = "All Time"
+
+    get_stock_price(st.session_state.ticker)
+    percentChange = calculatePercentageChange()
     
+    st.metric(label = "Current Stock Price of " + st.session_state.ticker + " - " + st.session_state.companyName, value=f"{st.session_state.currencySymbol} {st.session_state.stockPrice}", delta=f"{percentChange}% Past {st.session_state.plotName}")
+                
     if show_plot:
         plot_stock_price()
         st.image('stock.png')
         
     if show_EMA:
-        st.write("EMA:", calculate_EMA(30))
+        st.write("EMA:", str(calculate_EMA(30)))
         
     if show_SMA:
-        st.write("SMA:", calculate_SMA(30))
+        st.write("SMA:", str(calculate_SMA(30)))
 
     if show_rsi:
-        st.write("RSI:", calculate_RSI())
+        st.write("RSI:", str(calculate_RSI()))
 
     if show_macd:
         st.write("MACD:", calculate_MACD())
